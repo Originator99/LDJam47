25e5de4a6c4785b8b74261994b86157004c5badf
kassassin99 <kassassin99@gmail.com>
2020-10-03 21:44:40 +0530

    Bullet recochet

--- Assets/Scripts/Guns/Projectile.cs
+++ Assets/Scripts/Guns/Projectile.cs
@@ -7,17 +7,36 @@ public class Projectile : MonoBehaviour {
     public float lifeTime;
     public GameObject destroyEffect;
 
-    private void Update() {
+    [SerializeField]
+    LayerMask collisionMask;
+
+    private void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+    }
+
+    private void Update()
+    {
 
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, 0.25f);
-        if(hitInfo.collider != null) {
+        if (hitInfo.collider != null)
+        {
             Debug.Log("Bullet hit : " + hitInfo.collider.name);
-            if(hitInfo.collider.tag != "Player") {
+            if (hitInfo.collider.tag != "Player")
+            {
                 DestroyProjectile();
             }
         }
 
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, Time.deltaTime*speed+.1f, collisionMask);
+        if (hit.collider != null)
+        {
+            Vector2 reflectDir = Vector2.Reflect(transform.right, hit.normal);
+            float rot = Mathf.Atan2(reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0,0, rot);
+        }
     }
 
     private void DestroyProjectile() {
