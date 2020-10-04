3de51930f7e280d4df131622ed3fce33c14725c7
Originator99 <arsalaan.md0077@gmail.com>
2020-10-04 12:00:30 +0530

    timer fixes

---
+++ Assets/Scripts/Level/UI/TimerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+    public Text timerText;
+
+    private void Awake() {
+        GameEventSystem.GameEventHandler += HandleGameEvents;
+    }
+    private void OnDestroy() {
+        GameEventSystem.GameEventHandler -= HandleGameEvents;
+    }
+
+    private void HandleGameEvents(GAME_EVENT type, System.Object data = null) {
+        if(type == GAME_EVENT.LEVEL_START && data!=null && data.GetType() == typeof(float)) {
+        
+        }
+    }
+
+    private void OnLevelStart(float timer) {
+        
+    }
+}
